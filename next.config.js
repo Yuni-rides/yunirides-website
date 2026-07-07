42626383e5e88028dc71f9e54143528402af28da
shafyYuni <shafy@yunirides.com>
2026-07-08 04:51:49 +0500

    feat: Add for app url file for deeplinking

--- next.config.js
+++ next.config.js
@@ -12,6 +12,28 @@ const nextConfig = {
       },
     ];
   },
+  async headers() {
+    return [
+      {
+        source: '/.well-known/apple-app-site-association',
+        headers: [
+          {
+            key: 'Content-Type',
+            value: 'application/json',
+          },
+        ],
+      },
+      {
+        source: '/.well-known/assetlinks.json',
+        headers: [
+          {
+            key: 'Content-Type',
+            value: 'application/json',
+          },
+        ],
+      },
+    ];
+  },
 }
 
 export default nextConfig;
